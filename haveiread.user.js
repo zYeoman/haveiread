52bff05d7b350c42848026a07f7524ed3f1ddce8
Yongwen Zhuang <zyeoman@163.com>
2019-05-08 14:51:21 +0800

    :tophat: Use standard linter on haveiread.user.js

--- haveiread.user.js
+++ haveiread.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaveIRead
 // @namespace    https://mickir.me/
-// @version      0.6.3
+// @version      0.6.4
 // @description  Have I read this page?
 // @author       Mickir
 // @noframes
@@ -21,16 +21,16 @@
 // ==/UserScript==
 
 (async () => {
-  "use strict";
-  var username = await GM.getValue("user", "");
-  var key = await GM.getValue("key", "");
-  if (username == "") {
-    username = prompt("HaveIRead? Enter your username", "username");
-    key = prompt("Enter or Set your key", "key");
-    await GM.setValue("user", username);
-    await GM.setValue("key", key);
+  'use strict'
+  var username = await window.GM.getValue('user', '')
+  var key = await window.GM.getValue('key', '')
+  if (username === '') {
+    username = window.prompt('HaveIRead? Enter your username', 'username')
+    key = window.prompt('Enter or Set your key', 'key')
+    await window.GM.setValue('user', username)
+    await window.GM.setValue('key', key)
   }
-  GM.addStyle(`
+  window.GM.addStyle(`
     #haveiread {
       width: unset;
       padding: 0;
@@ -51,80 +51,80 @@
       padding: 0;
       outline: none;
     }
-    `);
+    `)
   // New Element
-  const show = document.createElement("div");
-  const input = document.createElement("p");
-  show.id = "haveiread";
-  input.contentEditable = true;
-  show.appendChild(input);
+  const show = document.createElement('div')
+  const input = document.createElement('p')
+  show.id = 'haveiread'
+  input.contentEditable = true
+  show.appendChild(input)
 
   // Request
-  function update(url) {
+  function update (url) {
     var data = {
       url: url || document.URL,
       title: document.title,
       user: username,
       key: key
-    };
-    GM.xmlHttpRequest({
-      method: "POST",
-      url: "https://api.mickir.me/read/",
+    }
+    window.GM.xmlHttpRequest({
+      method: 'POST',
+      url: 'https://api.mickir.me/read/',
       data: JSON.stringify(data),
       onload: response => {
-        var data = JSON.parse(response.responseText);
-        if (data.status == "OK") {
-          input.innerText = (data.read && (data.comment || "看过")) || "没看过";
-          show.style.color = (data.read && "red") || "green";
-          if (input.innerText != "n") {
-            show.style.display = "block";
+        var data = JSON.parse(response.responseText)
+        if (data.status === 'OK') {
+          input.innerText = (data.read && (data.comment || '看过')) || '没看过'
+          show.style.color = (data.read && 'red') || 'green'
+          if (input.innerText !== 'n') {
+            show.style.display = 'block'
           }
         } else {
         }
       }
-    });
+    })
   }
 
-  function comment(text) {
+  function comment (text) {
+    var url = document.URL
     var data = {
-      url: document.URL,
+      url: url,
       comment: text,
       user: username,
       key: key
-    };
-    var url = document.URL;
-    GM.xmlHttpRequest({
-      method: "PUT",
-      url: "https://api.mickir.me/comment/",
+    }
+    window.GM.xmlHttpRequest({
+      method: 'PUT',
+      url: 'https://api.mickir.me/comment/',
       data: JSON.stringify(data),
       onload: response => {
-        var data = JSON.parse(response.responseText);
-        if (data.status == "OK") {
-          input.blur();
+        var data = JSON.parse(response.responseText)
+        if (data.status === 'OK') {
+          input.blur()
         } else {
         }
       }
-    });
+    })
   }
 
-  document.body.appendChild(show);
-  update();
-  input.addEventListener("keypress", function(evt) {
+  document.body.appendChild(show)
+  update()
+  input.addEventListener('keypress', function (evt) {
     if (evt.which === 13) {
-      evt.preventDefault();
-      comment(input.innerText);
+      evt.preventDefault()
+      comment(input.innerText)
     }
-  });
-  function realURL(url) {
-    var a = document.createElement("a");
-    a.href = url;
-    return a.href;
+  })
+  function realURL (url) {
+    var a = document.createElement('a')
+    a.href = url
+    return a.href
   }
-  (function(history) {
-    var pushState = history.pushState;
-    history.pushState = function(state) {
-      update(realURL(arguments[2]));
-      return pushState.apply(history, arguments);
-    };
-  })(window.history);
-})();
+  (function (history) {
+    var pushState = history.pushState
+    history.pushState = function (state) {
+      update(realURL(arguments[2]))
+      return pushState.apply(history, arguments)
+    }
+  })(window.history)
+})()
